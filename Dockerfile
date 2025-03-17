14eab73771bc61ef591bfc7b32adf461184402d0
hgrange <hgrange@fr.ibm.com>
2025-03-17 17:11:44 +0100

    first commit

---
+++ Dockerfile
@@ -0,0 +1,31 @@
+FROM registry.access.redhat.com/ubi9/openjdk-21:latest AS builder
+WORKDIR /build
+ARG APP
+
+COPY --chown=185:0 . /build/$APP
+
+#RUN echo $APP && cd /build/$APP && tar -xf m2.tar && mvn -o clean package -Dmaven.repo.local=/build/$APP/.m2/repository
+RUN echo $APP && cd /build/$APP && mvn clean package 
+
+FROM icr.io/appcafe/websphere-liberty:25.0.0.2-kernel-java21-openj9-ubi-minimal
+ARG APP
+ARG TLS=true
+USER 0
+#RUN dnf install -y procps-ng && dnf clean all
+#RUN dnf update -y && dnf install -y curl tar gzip jq  procps util-linux vim-minimal iputils net-tools
+USER 1001
+
+COPY --from=builder --chown=1001:0  /build/$APP/target/*.*ar /config/apps/
+COPY --from=builder --chown=1001:0  /build/$APP/src/main/liberty/config/ /config/
+
+
+# This script will add the requested XML snippets to enable Liberty features and grow image to be fit-for-purpose using featureUtility.
+# Only available in 'kernel-slim'. The 'full' tag already includes all features for convenience.
+ENV VERBOSE=false
+
+RUN features.sh
+# Add interim fixes (optional)
+# COPY --chown=1001:0  interim-fixes /opt/ibm/wlp/fixes/
+
+# This script will add the requested server configurations, apply any interim fixes and populate caches to optimize runtime
+RUN bash configure.sh 
---
+++ src/main/java/com/acme/modres/mbean/DMBeanUtils.java
@@ -0,0 +1,44 @@
+// Assisted by watsonx Code Assistant
+package com.acme.modres.mbean;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.management.MBeanOperationInfo;
+
+public final class DMBeanUtils {
+	private static final Logger logger = Logger.getLogger(DMBeanUtils.class.getName());
+	
+	public static MBeanOperationInfo[] getOps(OpMetadataList opList) {
+		MBeanOperationInfo[] ops = null;
+		if (opList == null || opList.getOpMetadatList() == null) {
+			logger.log(Level.WARNING, "No operation is configured");
+			return ops;
+		}
+		
+		int numOps = opList.getOpMetadatList().size(); 
+		if (numOps > 0) {
+			ops = new MBeanOperationInfo[numOps];
+			int i = 0;
+			for (OpMetadata opMetadata : opList.getOpMetadatList()) {
+				String name = opMetadata.getName();
+				String desc = opMetadata.getDescription();
+				String type = opMetadata.getType();
+				int impact = opMetadata.getImpact();
+
+				try {
+					MBeanOperationInfo opInfo = new MBeanOperationInfo(name, desc, /* signature */ null, type, impact, /* descriptor */ null);
+					ops[i++] = opInfo;
+				} catch (IllegalArgumentException e) {
+					logger.log(Level.WARNING, "MBeanOperationInfo: {0} - impact value: {1} is invalid. Using impact value: {2}", 
+							new Object[]{name, impact, MBeanOperationInfo.UNKNOWN});
+					MBeanOperationInfo opInfo = new MBeanOperationInfo(name, desc, /* signature */ null, type, MBeanOperationInfo.UNKNOWN, /* descriptor */ null);
+					ops[i++] = opInfo;
+				}
+			}
+		}
+		
+		return ops;
+	}
+}
+
