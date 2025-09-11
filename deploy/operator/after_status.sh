a39e847bbde0e096b97d3ad64d812aa84cb68c55
hgrange <33756056+hgrange@users.noreply.github.com>
2025-09-11 16:14:45 +0200

    Update after_status.sh

--- deploy/operator/after_status.sh
+++ deploy/operator/after_status.sh
@@ -1,4 +1,4 @@
-#!/bin/bashA
+#!/bin/bash
 i=0
 while [ $(oc get WebSphereLibertyApplication modresorts -o jsonpath='{.status.conditions[*]}' | jq '. | select(.type == "Ready") | .status' | grep True |wc -l ) -eq 0 || i -lt 10 )  ]
   sleep 10
