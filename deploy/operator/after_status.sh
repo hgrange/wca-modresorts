0f2ea91e936e160141b9efc400892f8dfef72a48
hgrange <33756056+hgrange@users.noreply.github.com>
2025-09-11 17:20:57 +0200

    Update after_status.sh

--- deploy/operator/after_status.sh
+++ deploy/operator/after_status.sh
@@ -3,7 +3,7 @@ i=0
 status=1
 while [ $status -eq 1 ] && [ $i -lt 10 ]  
 do
-  status=$(oc get WebSphereLibertyApplication modresorts -o jsonpath='{.status.conditions[*]}' | jq '. | select(.type == "Ready") | .status' | grep False |wc -l )
+  status=$(oc get WebSphereLibertyApplication modresorts -o jsonpath='{.status.conditions[*]}' | tr '}' '\12' |grep \"Ready\" | tr ',' '\12' |grep status | grep False |wc -l )
   sleep 10
   echo -n .
   i=$(expr $i + 1)
